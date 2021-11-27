e79769e0c1a9450c30d2f54c6f9edf2a0f111100
trinhvu-realdude <vudangtrinh29102000@gmail.com>
2021-11-27 16:21:20 +0700

    update preventing strange characters

---
+++ js/core.js
@@ -0,0 +1,23 @@
+function go() {
+    const a_input = document.getElementById("a-value").value;
+    const b_input = document.getElementById("b-value").value;
+    const n_input = document.getElementById("n-value").value;
+
+    if (a_input != "" && b_input != "" && n_input != "") {
+        try {
+            const a = parseInt(a_input);
+            const b = parseInt(b_input);
+            const n = parseInt(n_input);
+
+            const d = gcd(n, a)[0];
+            const s = gcd(n, a)[1];
+            const r = gcd(n, a)[2];
+
+            extended_gcd(n, a, n / a, n % a, d);
+        } catch (e) {
+            throw e;
+        }
+    } else {
+        throw new SyntaxError();
+    }
+}
---
+++ js/filter.js
@@ -0,0 +1,28 @@
+(function ($) {
+    $.fn.inputFilter = function (inputFilter) {
+        return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function () {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                this.value = "";
+            }
+        });
+    };
+}(jQuery));
+
+$(document).ready(function () {
+    $("#a-value").inputFilter(function (value) {
+        return /^\d*$/.test(value);
+    });
+    $("#b-value").inputFilter(function (value) {
+        return /^\d*$/.test(value);
+    });
+    $("#n-value").inputFilter(function (value) {
+        return /^\d*$/.test(value);
+    });
+});
